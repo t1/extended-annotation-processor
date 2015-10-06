b19adcafb2a5141baf919c9c5301586e94df79a4
Rüdiger zu Dohna <snackbox@sinntr.eu>
2015-10-06 09:41:21 +0200

    Round#typesAnnotatedWith only process TYPE_KINDS

--- extended-annotation-processor/src/main/java/com/github/t1/exap/Round.java
+++ extended-annotation-processor/src/main/java/com/github/t1/exap/Round.java
@@ -1,14 +1,20 @@
 package com.github.t1.exap;
 
+import static java.util.Arrays.*;
+import static javax.lang.model.element.ElementKind.*;
+
 import java.lang.annotation.Annotation;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.processing.*;
-import javax.lang.model.element.Element;
+import javax.lang.model.element.*;
 
 import com.github.t1.exap.reflection.Type;
 
 public class Round {
+    private static final List<ElementKind> TYPE_KINDS = asList(ENUM, CLASS, ANNOTATION_TYPE, INTERFACE);
+
     private final ProcessingEnvironment processingEnv;
     private final RoundEnvironment roundEnv;
     private final int roundNumber;
@@ -22,7 +28,8 @@ public class Round {
     public List<Type> typesAnnotatedWith(Class<? extends Annotation> type) {
         List<Type> result = new ArrayList<>();
         for (Element element : roundEnv.getElementsAnnotatedWith(type))
-            result.add(Type.of(element.asType(), processingEnv));
+            if (TYPE_KINDS.contains(element.getKind()))
+                result.add(Type.of(element.asType(), processingEnv));
         return result;
     }
 
